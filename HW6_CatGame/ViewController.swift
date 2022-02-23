9e434d6310974e73863cfa6de82b60b7b1b29ee0
Anastasia Shalukho <nastya-shalukho@yandex.ru>
2022-02-24 00:07:31 +0300

    Initial commit

---
+++ HW6_CatGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6_CatGame
+//
+//  Created by Анастасия Шалухо on 23.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
